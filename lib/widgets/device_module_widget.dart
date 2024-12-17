5ff64522bd218a8b1df9e20282f602920fe4c693
Chris Coulthard <t00661081@mytru.ca>
2024-12-16 21:17:14 -0800

    - Updated device_module_widget.dart; Altered layout (WIP) and added 'Hover' and 'Pushed' states

--- lib/widgets/device_module_widget.dart
+++ lib/widgets/device_module_widget.dart
@@ -1,14 +1,14 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import '../app_constants.dart';
 
 class DeviceCrewHeader extends StatelessWidget {
-  const DeviceCrewHeader({super.key, required this.crew, required this.expanded});
+  const DeviceCrewHeader({super.key, required this.crew});
 
   final MapEntry<String, dynamic> crew;
-  final bool expanded;
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +17,7 @@ class DeviceCrewHeader extends StatelessWidget {
     return LayoutBuilder(
       builder: (BuildContext context, BoxConstraints constraints) {
         bool expandedView = constraints.maxWidth >= 300;
-        
+
         return AnimatedSwitcher(
           duration: const Duration(milliseconds: 150),
           transitionBuilder: (Widget child, Animation<double> animation) {
@@ -28,88 +28,130 @@ class DeviceCrewHeader extends StatelessWidget {
           },
           child: expandedView
               ? Container(
-            height: 45,
-            decoration: BoxDecoration(
-              color: themeDarkAccentColourMain,
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: Center(
-              child: Text(
-                crew.key,
-                style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: themeDarkPrimaryText,
-                  fontSize: 24,
-                ),
-                maxLines: 1,
-              ),
-            ),
-          )
+                  height: 45,
+                  decoration: BoxDecoration(
+                    color: themeDarkAccentColourMain,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Center(
+                    child: Text(
+                      crew.key,
+                      style: GoogleFonts.varelaRound(
+                        fontWeight: FontWeight.bold,
+                        color: themeDarkPrimaryText,
+                        fontSize: 24,
+                      ),
+                      textAlign: TextAlign.center,
+                      maxLines: 1,
+                    ),
+                  ),
+                )
               : Center(
-            child: Container(
-              height: 45,
-              width: 45,
-              decoration: BoxDecoration(
-                color: themeDarkAccentColourMain,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Center(
-                child: Text(
-                  crewInitials,
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: themeDarkSecondaryText,
-                    fontSize: 24,
+                  child: Container(
+                    height: 45,
+                    width: 45,
+                    decoration: BoxDecoration(
+                      color: themeDarkAccentColourMain,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Center(
+                      child: Text(
+                        crewInitials,
+                        style: GoogleFonts.varelaRound(
+                          fontWeight: FontWeight.bold,
+                          color: themeDarkSecondaryText,
+                          fontSize: 24,
+                        ),
+                      ),
+                    ),
                   ),
                 ),
-              ),
-            ),
-          ),
         );
       },
     );
   }
 }
 
-class DeviceMemberModule extends StatelessWidget {
-  const DeviceMemberModule({super.key, required this.member, required this.expanded});
+class DeviceMemberModule extends StatefulWidget {
+  const DeviceMemberModule({super.key, required this.member});
 
   final MapEntry<String, dynamic> member;
-  final bool expanded;
+
+  @override
+  State<DeviceMemberModule> createState() => _DeviceMemberModuleState();
+}
+
+class _DeviceMemberModuleState extends State<DeviceMemberModule> {
+  bool _isHovered = false;
+  bool _isPressed = false;
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (BuildContext context, BoxConstraints constraints) {
-        bool expandedView = constraints.maxWidth >= 300;
-
-        return Container(
-          height: 65,
+    return Listener(
+      onPointerDown: (_) {
+        setState(() {
+          _isPressed = true;
+        });
+      },
+      onPointerUp: (_) {
+        setState(() {
+          _isPressed = false;
+          //widget.buttonFunction();
+        });
+      },
+      onPointerCancel: (_) {
+        setState(() {
+          _isPressed = false;
+        });
+      },
+      child: MouseRegion(
+        onEnter: (_) {
+          setState(() {
+            _isHovered = true;
+          });
+        },
+        onExit: (_) {
+          setState(() {
+            _isHovered = false;
+          });
+        },
+        child: AnimatedContainer(
+          height: 60,
           decoration: BoxDecoration(
-            color: themeDarkBackground,
-            borderRadius: BorderRadius.circular(25),
+            color: Colors.white.withOpacity((_isPressed
+                ? 0.08
+                : _isHovered
+                    ? 0.15
+                    : 0.1)),
+            borderRadius: BorderRadius.circular(10),
           ),
+          duration: const Duration(milliseconds: 100),
           child: SingleChildScrollView(
             scrollDirection: Axis.horizontal,
             physics: const NeverScrollableScrollPhysics(),
             child: Row(
               children: [
                 Padding(
-                  padding: const EdgeInsets.all(5),
-                  child: Container(
+                  padding: const EdgeInsets.all(3),
+                  child: AnimatedContainer(
                     height: 55,
                     width: 65,
                     decoration: BoxDecoration(
-                      color: themeDarkForeground,
-                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.white.withOpacity((_isPressed
+                          ? 0.08
+                          : _isHovered
+                              ? 0.15
+                              : 0.1)),
+                      borderRadius: BorderRadius.circular(8),
                     ),
-                    child: const Center(
+                    duration: const Duration(milliseconds: 100),
+                    child: Center(
                       child: Text(
-                        "1",
-                        style: TextStyle(
+                        widget.member.key,
+                        style: GoogleFonts.varelaRound(
                           fontWeight: FontWeight.w900,
                           color: themeDarkDimText,
-                          fontSize: 28,
+                          fontSize: 20,
                         ),
                       ),
                     ),
@@ -123,16 +165,8 @@ class DeviceMemberModule extends StatelessWidget {
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Text(
-                          member.key,
-                          style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: themeDarkPrimaryText,
-                            fontSize: 18,
-                          ),
-                        ),
-                        Text(
-                          "${member.value['lastName']}",
-                          style: const TextStyle(
+                          "${widget.member.value['lastName']}",
+                          style: GoogleFonts.varelaRound(
                             fontWeight: FontWeight.bold,
                             color: themeDarkDimText,
                             fontSize: 12,
@@ -145,8 +179,8 @@ class DeviceMemberModule extends StatelessWidget {
               ],
             ),
           ),
-        );
-      },
+        ),
+      ),
     );
   }
 }
