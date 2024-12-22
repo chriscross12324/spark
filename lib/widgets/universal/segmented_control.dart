6f959fc791bd9453922aec1fe5d577782088772c
Chris Coulthard <t00661081@mytru.ca>
2024-12-22 00:46:55 -0800

    - Updated segmented_control.dart; Added hover effect to buttons

--- lib/widgets/universal/segmented_control.dart
+++ lib/widgets/universal/segmented_control.dart
@@ -32,33 +32,81 @@ class _SegmentedControlState extends State<SegmentedControl> {
                 selectedIndex = index;
               });
             },
-            child: AnimatedContainer(
-              height: 36,
-              duration: const Duration(milliseconds: 200),
-              decoration: BoxDecoration(
-                color: isSelected ? themeDarkForeground : Colors.transparent,
-                borderRadius: BorderRadius.circular(6),
-                border: Border.all(
-                  width: 2,
-                  color: isSelected ? themeDarkDimText : Colors.transparent,
-                ),
-              ),
-              padding: const EdgeInsets.symmetric(horizontal: 12),
-              child: Center(
-                child: AnimatedDefaultTextStyle(
-                  style: GoogleFonts.asap(
-                    fontWeight:
-                        isSelected ? FontWeight.bold : FontWeight.normal,
-                    color: isSelected ? themeDarkPrimaryText : themeDarkDimText,
-                    fontSize: 14,
-                  ),
-                  duration: const Duration(milliseconds: 200),
-                  child: Text(widget.options[index]),
-                ),
-              ),
+            child: SegmentedControlButton(
+              isSelected: isSelected,
+              widget: widget,
+              buttonText: widget.options[index],
             ),
           );
         }),
+      ),
+    );
+  }
+}
+
+class SegmentedControlButton extends StatefulWidget {
+  const SegmentedControlButton({
+    super.key,
+    required this.isSelected,
+    required this.widget,
+    required this.buttonText,
+  });
+
+  final bool isSelected;
+  final SegmentedControl widget;
+  final String buttonText;
+
+  @override
+  State<SegmentedControlButton> createState() => _SegmentedControlButtonState();
+}
+
+class _SegmentedControlButtonState extends State<SegmentedControlButton> {
+  bool _isHovered = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      onEnter: (_) {
+        setState(() {
+          _isHovered = true;
+        });
+      },
+      onExit: (_) {
+        setState(() {
+          _isHovered = false;
+        });
+      },
+      child: AnimatedContainer(
+        height: 36,
+        duration: const Duration(milliseconds: 200),
+        decoration: BoxDecoration(
+          color: widget.isSelected
+              ? themeDarkForeground
+              : _isHovered
+                  ? themeDarkForeground.withOpacity(0.25)
+                  : Colors.transparent,
+          borderRadius: BorderRadius.circular(6),
+          border: Border.all(
+            width: 2,
+            color: widget.isSelected
+                ? themeDarkDimText
+                : Colors.transparent,
+          ),
+        ),
+        padding: const EdgeInsets.symmetric(horizontal: 12),
+        child: Center(
+          child: AnimatedDefaultTextStyle(
+            style: GoogleFonts.asap(
+              fontWeight:
+                  widget.isSelected ? FontWeight.bold : FontWeight.normal,
+              color:
+                  widget.isSelected ? themeDarkPrimaryText : themeDarkDimText,
+              fontSize: 14,
+            ),
+            duration: const Duration(milliseconds: 200),
+            child: Text(widget.buttonText),
+          ),
+        ),
       ),
     );
   }
