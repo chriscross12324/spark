eb77995d86b9b0461cd01bd4dff690cb2ee694a1
Chris Coulthard <t00661081@mytru.ca>
2024-12-22 22:49:18 -0800

    - Updated text_button_widget.dart; Migrated to use MouseEffectsContainer widget for state effects.

--- lib/widgets/universal/text_button_widget.dart
+++ lib/widgets/universal/text_button_widget.dart
@@ -1,98 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:spark/app_constants.dart';
+import 'package:spark/widgets/universal/mouse_effects.dart';
 
 /// A custom TextButton widget
-class TextButtonWidget extends StatefulWidget {
-  const TextButtonWidget(
-      {super.key,
-      required this.text,
-      required this.onPressed,
-      this.isPrimary = false,
-      this.isButtonClear = false,
-      this.containsPadding = true});
+class TextButtonWidget extends StatelessWidget {
+  const TextButtonWidget({
+    super.key,
+    required this.text,
+    required this.onPressed,
+    this.isPrimary = false,
+    this.isButtonClear = false,
+    this.containsPadding = true,
+  });
 
   // Button Properties
   final String text;
-  final Function onPressed;
+  final VoidCallback onPressed;
   final bool isPrimary;
   final bool isButtonClear;
   final bool containsPadding;
 
   @override
-  State<TextButtonWidget> createState() => _TextButtonWidgetState();
-}
-
-class _TextButtonWidgetState extends State<TextButtonWidget> {
-  bool _isButtonHovered = false;
-  bool _isButtonPressed = false;
-
-  @override
   Widget build(BuildContext context) {
-    return Listener(
-      onPointerDown: (_) {
-        setState(() {
-          _isButtonPressed = true;
-        });
-      },
-      onPointerUp: (_) {
-        setState(() {
-          _isButtonPressed = false;
-          widget.onPressed();
-        });
-      },
-      onPointerCancel: (_) {
-        setState(() {
-          _isButtonPressed = false;
-        });
-      },
-      child: MouseRegion(
-        onEnter: (_) {
-          setState(() {
-            _isButtonHovered = true;
-          });
-        },
-        onExit: (_) {
-          setState(() {
-            _isButtonHovered = false;
-          });
-        },
-        child: AnimatedContainer(
-          height: 40,
-          duration: const Duration(milliseconds: 100),
-          decoration: BoxDecoration(
-            color: widget.isPrimary
-                ? (const Color(0xFF0080FF).withOpacity((_isButtonPressed
-                    ? 0.1
-                    : _isButtonHovered
-                        ? 0.45
-                        : widget.isButtonClear
-                            ? 0
-                            : 0.25)))
-                : (themeDarkPrimaryText.withOpacity(
-                    (_isButtonPressed
-                        ? 0.05
-                        : _isButtonHovered
-                            ? 0.25
-                            : widget.isButtonClear
-                                ? 0
-                                : 0.1),
-                  )),
-            borderRadius: BorderRadius.circular(10),
-          ),
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: widget.containsPadding ? 38.0 : 10),
-            child: Center(
-              child: Text(
-                widget.text,
-                style: GoogleFonts.asap(
-                  fontWeight: widget.isPrimary ? FontWeight.w900 : FontWeight.w500,
-                  color: widget.isPrimary
-                      ? const Color(0xFF0095FF)
-                      : themeDarkPrimaryText,
-                  fontSize: 14,
-                ),
-              ),
+    return MouseEffectsContainer(
+      height: 40,
+      opacity: isButtonClear
+          ? 0.0
+          : isPrimary
+              ? 0.25
+              : 0.1,
+      opacityAdd: 0.15,
+      opacitySubtract: isButtonClear
+          ? 0.0
+          : isPrimary
+              ? 0.15
+              : 0.05,
+      color: isPrimary ? const Color(0xFF0080FF) : Colors.white,
+      onPressed: onPressed,
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: containsPadding ? 38.0 : 10),
+        child: Center(
+          child: Text(
+            text,
+            style: GoogleFonts.asap(
+              fontWeight: isPrimary ? FontWeight.w900 : FontWeight.w500,
+              color: isPrimary ? const Color(0xFF0095FF) : themeDarkPrimaryText,
+              fontSize: 14,
             ),
           ),
         ),
