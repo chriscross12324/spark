c5de74c86517bce139e4e0453dca570f71464720
Chris Coulthard <t00661081@mytru.ca>
2025-04-01 00:58:07 -0700

    - Updated device_list_widget.dart; Updated to work with new Device Group Providers

--- lib/pages/dashboard/widgets/device_list_widget.dart
+++ lib/pages/dashboard/widgets/device_list_widget.dart
@@ -8,29 +8,35 @@ import 'package:spark/providers/dashboard_provider.dart';
 import 'package:spark/utils/web_socket_manager.dart';
 import 'package:spark/widgets/common/mouse_effects.dart';
 
+import '../../../models/device.dart';
+
 class DeviceListWidget extends ConsumerWidget {
   const DeviceListWidget({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final listState = ref.watch(dashboardProvider);
+    final asyncDeviceGroups = ref.watch(groupProvider).value ?? [];
+    if (asyncDeviceGroups.isEmpty) {
+      return Center(
+        child: Text('No groups available. Add a group to get started!'),
+      );
+    }
 
     return ListView.builder(
       padding: const EdgeInsets.symmetric(horizontal: 10),
-      itemCount: listState.crewGroups
-          .fold<int>(0, (count, group) => count + group.members.length + 2),
+      itemCount: asyncDeviceGroups.fold<int>(0, (count, group) => count + group.groupDevices.length + 2),
       itemBuilder: (context, index) {
         int currentCount = 0;
 
-        for (final group in listState.crewGroups) {
+        for (final group in asyncDeviceGroups) {
           if (index == currentCount) {
             return CrewHeaderItem(crew: group);
           }
           currentCount++;
 
-          for (int i = 0; i < group.members.length; i++) {
+          for (int i = 0; i < group.groupDevices.length; i++) {
             if (index == currentCount) {
-              return MemberItem(crew: group, member: group.members[i]);
+              return MemberItem(crew: group, member: group.groupDevices[i]);
             }
             currentCount++;
           }
@@ -53,7 +59,7 @@ class DeviceListWidget extends ConsumerWidget {
 class CrewHeaderItem extends StatelessWidget {
   const CrewHeaderItem({super.key, required this.crew});
 
-  final CrewGroup crew;
+  final Group crew;
 
   @override
   Widget build(BuildContext context) {
@@ -65,7 +71,7 @@ class CrewHeaderItem extends StatelessWidget {
       ),
       child: Center(
         child: Text(
-          crew.name,
+          crew.groupName,
           style: GoogleFonts.asap(
             fontWeight: FontWeight.bold,
             color: themeDarkPrimaryText,
@@ -86,17 +92,19 @@ class MemberItem extends ConsumerWidget {
     required this.member,
   });
 
-  final CrewGroup crew;
-  final CrewMember member;
+  final Group crew;
+  final Device member;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final listState = ref.watch(dashboardProvider);
-    final stateNotifier = ref.read(dashboardProvider.notifier);
+    final selectedDeviceWatcher = ref.watch(selectedDeviceProvider);
+    final selectedDeviceReader = ref.read(selectedDeviceProvider.notifier);
     final wsManager = ref.read(webSocketManagerProvider.notifier);
     final displayDeviceStatus = ref.watch(settingDisplayDeviceStatus);
 
-    final isLastMember = crew.members.last == member;
+    final isLastMember = crew.groupDevices.last == member;
+
+    print("Selected Device: $selectedDeviceWatcher");
 
     return Column(
       children: [
@@ -105,18 +113,18 @@ class MemberItem extends ConsumerWidget {
           child: MouseEffectsContainer(
             height: 60,
             onPressed: () {
-              if (member == listState.selectedMember) {
+              if (member == selectedDeviceWatcher) {
                 wsManager.disconnect();
-                stateNotifier.selectItem(null);
+                selectedDeviceReader.clearSelection();
               } else {
-                wsManager.connect(member.id);
-                stateNotifier.selectItem(member);
+                wsManager.connect(member.deviceID);
+                selectedDeviceReader.selectDevice(member);
               }
             },
-            border: listState.selectedMember == member
+            border: selectedDeviceWatcher == member
                 ? Border.all(color: Colors.white, width: 2)
                 : null,
-            opacity: listState.selectedMember == member ? 0.15 : 0.1,
+            opacity: selectedDeviceWatcher == member ? 0.15 : 0.1,
             spotlightRadius: 200,
             child: Padding(
               padding: const EdgeInsets.all(3),
@@ -128,12 +136,12 @@ class MemberItem extends ConsumerWidget {
                     decoration: BoxDecoration(
                       color: Colors.white.withOpacity(0.1),
                       borderRadius: BorderRadius.circular(
-                        listState.selectedMember == member ? 5 : 7,
+                        selectedDeviceWatcher == member ? 5 : 7,
                       ),
                     ),
                     child: Center(
                       child: Text(
-                        member.id,
+                        member.deviceID,
                         style: GoogleFonts.asap(
                           fontWeight: FontWeight.w900,
                           color: themeDarkDimText,
@@ -149,12 +157,12 @@ class MemberItem extends ConsumerWidget {
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Text(
-                          member.lastName,
+                          member.deviceUserName,
                           style: GoogleFonts.asap(
-                            fontWeight: listState.selectedMember == member
+                            fontWeight: selectedDeviceWatcher == member
                                 ? FontWeight.w900
                                 : FontWeight.bold,
-                            color: listState.selectedMember == member
+                            color: selectedDeviceWatcher == member
                                 ? themeDarkSecondaryText
                                 : themeDarkDimText,
                             fontSize: 14,
@@ -169,9 +177,9 @@ class MemberItem extends ConsumerWidget {
                       child: Container(
                         width: 4,
                         decoration: BoxDecoration(
-                          color: member.status == 'Online'
+                          color: member.deviceStatus == 'Online'
                               ? Colors.green
-                              : member.status == 'Offline'
+                              : member.deviceStatus == 'Offline'
                                   ? Colors.white.withOpacity(0.35)
                                   : Colors.red,
                           borderRadius: BorderRadius.circular(2),
